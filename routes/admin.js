51dd7f99e12c84e7d6d263a5a433c35656f1b4dd
Amir Bawab <amirbawab@gmail.com>
2017-12-28 12:35:10 -0500

    Added web config

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var template = require('../helper/template');
 
+router.get('/config', function(req, res, next) {
+    var path = [{name: 'Admin', url: '/admin'},{name: 'Configuration'}];
+    template.render(res, 'admin/config', 'Website configuration', { _path: path});
+});
+
 router.get('/', function(req, res, next) {
     var path = [{name: 'Admin', url: '/admin'},{name: 'Home'}];
     template.render(res, 'admin/home', 'Admin panel', { _path: path});
