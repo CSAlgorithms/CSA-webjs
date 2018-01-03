2928a0a482e96e6906031c3cb1db4fbc78cda5d7
Amir Bawab <amirbawab@gmail.com>
2018-01-02 20:01:13 -0500

    Added event ajax logic

--- routes/ajax/event.js
+++ routes/ajax/event.js
@@ -5,7 +5,21 @@ var log = require('winston');
 var _ = require('lodash');
 
 router.post('/add_question', function(req, res, next) {
-    res.json({hello: 'world'});
+    var body = _.pick(req.body, ['e_id', 'q_id']);
+    Event.findById(body.e_id).then(function (event) {
+        if(_.isNull(event)) {
+            // TODO replace numbers with CODES
+            res.json({code: 404});
+        } else {
+            event.questions.addToSet(body.q_id);
+            event.save().then(function () {
+                res.json({code: 200});
+            }).catch(function (reason) {
+                log.error('Error assigning question to event: ' + reason);
+                res.json({code: 500});
+            });
+        }
+    });
 });
 
 module.exports = router;
