e4c7d6d262181c6bba27f2b1c0669319248d22c6
Amir Bawab <amirbawab@gmail.com>
2018-01-03 12:32:37 -0500

    Fixed bug in ajax call

--- routes/ajax/event.js
+++ routes/ajax/event.js
@@ -6,13 +6,14 @@ var _ = require('lodash');
 var HTTP = require('http-status-codes');
 
 router.post('/add_question', function(req, res, next) {
-    var body = _.pick(req.body, ['e_id', 'q_id']);
-    Event.findById(body.e_id).then(function (event) {
+    var body = _.pick(req.body, ['eid', 'qid']);
+    Event.findById(body.eid).then(function (event) {
         if(_.isNull(event)) {
             res.json({code: HTTP.NOT_FOUND});
         } else {
-            event.questions.addToSet(body.q_id);
+            event.questions.addToSet(body.qid);
             event.save().then(function () {
+                log.info('Question ' + body.qid + ' assigned to event ' + body.eid);
                 res.json({code: HTTP.OK});
             }).catch(function (reason) {
                 log.error('Error assigning question to event: ' + reason);
@@ -23,17 +24,18 @@ router.post('/add_question', function(req, res, next) {
 });
 
 router.post('/remove_question', function(req, res, next) {
-    var body = _.pick(req.body, ['e_id', 'q_id']);
-    Event.findById(body.e_id).then(function (event) {
+    var body = _.pick(req.body, ['eid', 'qid']);
+    Event.findById(body.eid).then(function (event) {
         if(_.isNull(event)) {
             res.json({code: HTTP.NOT_FOUND});
         } else {
-            var index = event.questions.indexOf(body.q_id);
+            var index = event.questions.indexOf(body.qid);
             if(index < 0) {
                 res.json({code: HTTP.BAD_REQUEST});
             } else {
                 event.questions.splice(index, 1);
                 event.save().then(function () {
+                    log.info('Question ' + body.qid + ' removed from event ' + body.eid);
                     res.json({code: HTTP.OK});
                 }).catch(function (reason) {
                     log.error('Error removing question from event: ' + reason);
