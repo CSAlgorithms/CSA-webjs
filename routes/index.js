8621ac1a9e3abe0f84d83118394068232d542388
Amir Bawab <amirbawab@gmail.com>
2018-01-11 07:34:24 -0500

    Added latest activities

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Activity = require('../models/activity').Activity;
+var ActivityHelper = require('../helpers/ActivityHelper');
+var Question = require('../models/question').Question;
 
 router.get('/', function(req, res, next) {
-    res.setPath([{name: "Home"}]);
-    res.templateRender('home/home', 'Welcome page');
+    Activity.find().sort({createdAt: -1}).limit(20).populate('user').populate('object').then(function(activities) {
+        res.addData('activities', ActivityHelper.toHtmlArray(activities));
+        res.setPath([{name: "Home"}]);
+        res.templateRender('home/home', 'Welcome page');
+    });
 });
 
 module.exports = router;
