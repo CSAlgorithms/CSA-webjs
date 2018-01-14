6a73fc6a5f414913c252bb7dbf73efdb7737c2d2
Amir Bawab <amirbawab@gmail.com>
2018-01-14 17:04:42 -0500

    Added submission view

--- routes/submission.js
+++ routes/submission.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var _ = require('lodash');
 var Submission = require('../models/submission').Submission;
 var ManualSubmission = require('../models/submission').ManualSubmission;
+var CompareSubmission = require('../models/submission').CompareSubmission;
+var DockerSubmission = require('../models/submission').DockerSubmission;
 var auth = require('../config/auth');
 var Question = require('../models/question').Question;
+var constant = require('../config/constants').STATIC;
 
 router.get('/question/:id(\\d+)', auth.loggedin, function(req, res, next) {
     var me = res.getData('me');
@@ -60,6 +63,30 @@ router.get('/pending', auth.admin, function(req, res, next) {
         res.addData('submissions', submissions);
         res.loadScript('dataTable');
         res.templateRender('submission/pending', 'Pending submissions');
+    });
+});
+
+router.get('/view/:id(\\d+)', auth.admin, function(req, res, next) {
+    Submission.findOne({sid: req.params.id}).populate('question').populate({
+        path: 'type',
+        populate: {
+            path: 'language'
+        }
+    }).then(function(submission) {
+        if(_.isNull(submission)) {
+            res.redirect404('Submission not found');
+        } else {
+            res.addData('submission', submission);
+            if(submission.type instanceof ManualSubmission) {
+                res.addData('submissionType', constant.SUBMIT_TYPE_MANUAL);
+            } else if(submission.type instanceof CompareSubmission) {
+                res.addData('submissionType', constant.SUBMIT_TYPE_COMPARE);
+            } else if(submission.type instanceof DockerSubmission) {
+                res.addData('submissionType', constant.SUBMIT_TYPE_DOCKER);
+            }
+            res.loadScript('ace');
+            res.templateRender('submission/view', 'View submission');
+        }
     });
 });
 
